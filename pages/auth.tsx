d82ffaddac120c31e00128878f344356b7f5d320
ajml-code <al2blues@gmail.com>
2024-02-15 20:00:29 +0000

    Stable

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,133 @@
-const Auth = () => {
-    return (
-     <div className="relative h-full w-full bg[url('/images/hero.jpg')] bg-no-repeat bg-centre bg-fixed bg-cover">
+import Input from "@/components/input";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import {signIn} from 'next-auth/react';
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+const Auth = () => {
+
+    const[email, setEmail] = useState('');
+    const[name, setName] = useState('');
+    const[password, setPassword] = useState('');
+
+    const[varient, setVariant] = useState('login');
+
+    const toggleVariant = useCallback (() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, []);
+
+    const login = useCallback(async () => {
+        try{
+            await signIn('credentials', {
+                email,
+                password,
+                callbackUrl: '/profiles'
+            });
+        }catch(error){
+            console.log(error);
+        }
+    }, [email, password]);
+
+    const register = useCallback(async ()=> {
+        try {
+            await axios.post('/api/register',{
+                email,
+                name,
+                password
+            });
+
+            login();
+        } catch (error){
+            console.log(error);
+        }
+    }, [email,name,password, login]);
+
+    
+
+    return (
+     <div className="relative h-full w-full bg-hero bg-no-repeat bg-center bg-fixed bg-cover">
+        <div className="bg-black w-full h-full lg:bg-opacity-50">
+            <nav className="px-12 py-5">
+                <img src="/images/logo.png" alt="logo" className="h-12"/>
+            </nav>
+            <div className="flex justify-center">
+                <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    <h2 className="text-white text-4xl mb-8 font-semibold">
+                        {varient === 'login' ? 'Sign in' : 'Register'}
+                    </h2>
+                    <div className="flex flex-col gap-4">
+                        {varient === 'register' && (
+                            <Input 
+                                label="Username"
+                                onChange={(ev:any)=>setName(ev.target.value)}
+                                id="name"
+                                type=""
+                                value={name}
+                            />
+                        )}
+                        <Input 
+                            label="Email"
+                            onChange={(ev:any)=>setEmail(ev.target.value)}
+                            id="email"
+                            type="email"
+                            value={email}
+                        />
+                        <Input 
+                            label="Password"
+                            onChange={(ev:any)=>setPassword(ev.target.value)}
+                            id="password"
+                            type="password"
+                            value={password}
+                        />
+                    </div>
+                    <button  onClick={varient === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        {varient === 'login' ?'Login' : 'Sign up'}
+                    </button>
+                    <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                        <div
+                            className="
+                            w-10
+                            h-10
+                            bg-white
+                            rounded-full
+                            flex
+                            items-center
+                            justify-center
+                            cursor-pointor
+                            hover:opacity-80
+                            transition
+                            ">
+                                <FcGoogle  size={30}/>
+                        </div>
+                        <div
+                            
+                            className="
+                            w-10
+                            h-10
+                            bg-white
+                            rounded-full
+                            flex
+                            items-center
+                            justify-center
+                            cursor-pointor
+                            hover:opacity-80
+                            transition
+                            ">
+                                <FaGithub  size={30}/>
+                        </div>
+
+                    </div>
+                    <p className="text-neutral-500 mt-12">
+                        {varient === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+                        <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                            {varient === 'login' ? 'Create an account': 'Login'}
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
      </div>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,49 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import  Navbar  from '@/components/Navbar';
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavourite";
+import InfoModal from "@/components/InfoModal";
+import useInfoModal from "@/hooks/useInfoModal";
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if (!session){
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+
 export default function Home() {
+
+  const {data : movies = []} = useMovieList();
+  const {data : favourites = []} = useFavorites();
+  const {isOpen, closeModal} = useInfoModal();
+
   return (
     <>
-    <h1 className="text-2x1 text-green-500">netflix clone</h1>
-    </>
-   
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div>
+        <MovieList title="Trending Now" data={movies}/>
+        <MovieList title="My List" data={favourites}/>
 
+      </div>
+      
+    </>
   );
 }
