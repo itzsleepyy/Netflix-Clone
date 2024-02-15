3656e4b7362297a17c2dec2b234e77cdd0f70675
ajml-code <al2blues@gmail.com>
2024-02-15 20:34:44 +0000

    github & Google auth fixed

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -87,6 +87,7 @@ const Auth = () => {
                     </button>
                     <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                         <div
+                            onClick={() => signIn('google', {callbackUrl: '/'})}
                             className="
                             w-10
                             h-10
@@ -102,7 +103,7 @@ const Auth = () => {
                                 <FcGoogle  size={30}/>
                         </div>
                         <div
-                            
+                            onClick={() => signIn('github', {callbackUrl: '/'})}
                             className="
                             w-10
                             h-10
